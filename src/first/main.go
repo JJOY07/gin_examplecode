0303aeb530289e7575bfdd85570055055261f170
JJOY07 <ebeee.jjoy@gmail.com>
2018-11-24 17:50:29 -0800

    gin router

--- src/first/main.go
+++ src/first/main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(context *gin.Context) {
-		context.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	g := router.Group("/beee")
+	{
+		g.GET("/health", health)
+		g.POST("/signup", signup)
+		g.POST("/login", login)
+	}
+
 	router.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func health(cxt *gin.Context) {
+	cxt.JSON(http.StatusOK, gin.H{
+		"message": "OK",
+	})
+}
+
+func signup(cxt *gin.Context) {
+	cxt.JSON(http.StatusCreated, gin.H{
+		"message": "signed up",
+	})
+}
+
+func login(cxt *gin.Context) {
+	cxt.JSON(http.StatusOK, gin.H{
+		"message": "logged in",
+	})
 }
